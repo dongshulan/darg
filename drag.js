9a7d4d8a80012bc6c65c4888658f539f07dbdda7
dongshulan <984925816@qq.com>
2017-02-25 15:45:44 +0800

    chongxin apply

---
+++ drag.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Administrator on 2017/2/23.
+ */
+function(id){
+    var obj=document.getElementById(id);
+    var disX=0;
+    var disY=0;
+    obj.onmousedown=function(ev){
+        disX=ev.pageX-obj.offsetLeft;
+        disY=ev.pageY-obj.offsetTop;
+        document.onmousemove=fucntion(){
+            obj.style.left=ev.pageX - disX + 'px';
+            obj.style.top=ev.pageY - disY + 'px';
+        };
+    };
+};
